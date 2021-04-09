a4c6e81dd61857986bf328ada7508d46f0a57a10
Brian Vanderbusch <chief@hackerhappyhour.com>
2021-04-09 12:40:30 -0500

    setup a blinky pin

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,35 @@
 use stm32f3xx_hal as _;
 use panic_halt as _;
 
-#[rtic::app(device = stm32f3xx_hal::stm32, dispatchers=[USART1_EXTI25])]
+#[rtic::app(device = stm32f3xx_hal::stm32, 
+  peripherals = true, 
+  dispatchers=[USART1_EXTI25])]
 mod app {
   use rtt_target::{ rprintln, rtt_init_print };
+  use stm32f3xx_hal::{
+    gpio::{gpiob::PB13, Output, PushPull},
+    prelude::*
+  };
+
+  #[resources]
+  struct Resources {
+    led: PB13<Output<PushPull>>,
+  }
+
   #[init]
   fn init(cx: init::Context) -> (init::LateResources, init::Monotonics) {
-    let _device: stm32f3xx_hal::stm32::Peripherals = cx.device;
+
+    let pac = cx.device;
+    let mut rcc = pac.RCC.constrain();
+    let mut gpiob = pac.GPIOB.split(&mut rcc.ahb);
+
+    let mut led = gpiob.pb13.into_push_pull_output(&mut gpiob.moder, &mut gpiob.otyper);
+
+    led.set_low().ok();
+
     rtt_init_print!();
-    rprintln!("Initialized...");
-   ( init::LateResources {}, init::Monotonics())
+    rprintln!("Initialization complete");
+   ( init::LateResources { led, }, init::Monotonics())
   }
 
       // Optional idle task, if left out idle will be a WFI.
